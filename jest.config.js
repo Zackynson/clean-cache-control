cb886a7021b9c42eee596405b3993b8d6e3fea8e
Zackynson <crystopher.carvalho@universo.univates.br>
2021-09-17 09:27:41 -0300

    feat: config eslint, ts and jest

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  roots: [
+    '<rootDir>/src',
+  ],
+  testEnviroment: 'node',
+  transform: {
+    '.*\\.ts$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1',
+  },
+};
