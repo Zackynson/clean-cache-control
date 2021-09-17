72df0d5c87b7f04a1596ffbb234fecba9b2e66e2
Zackynson <crystopher.carvalho@universo.univates.br>
2021-09-17 10:03:10 -0300

    ensure LocalSavePurchases delete old Cache on save

--- src/data/usecases/local-save-purchases.spec.ts
+++ src/data/usecases/local-save-purchases.spec.ts
@@ -1,15 +1,24 @@
 // eslint-disable-next-line max-classes-per-file
 interface CacheStore {
-
+  delete: () => void
 }
 
 class CacheStoreSpy implements CacheStore {
   deleteCallsCount = 0;
+
+  delete(): void {
+    this.deleteCallsCount += 1;
+  }
 }
 
 class LocalSavePurchases {
   constructor(private readonly cacheStore: CacheStore) {
     this.cacheStore = cacheStore;
+  }
+
+  async save(): Promise<void> {
+    this.cacheStore.delete();
+    return Promise.resolve();
   }
 }
 
@@ -19,5 +28,14 @@ describe('LocalSavePurchases', () => {
     new LocalSavePurchases(cacheStore);
 
     expect(cacheStore.deleteCallsCount).toBe(0);
+  });
+
+  test('Should delete old Cache on sut.save', () => {
+    const cacheStore = new CacheStoreSpy();
+    const sut = new LocalSavePurchases(cacheStore);
+
+    sut.save();
+
+    expect(cacheStore.deleteCallsCount).toBe(1);
   });
 });
